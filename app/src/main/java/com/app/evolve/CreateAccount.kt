5172b14ca847ee175bebf53b7a68cfc8121e54a5
ashimsaini <ashimsaini37@gmail.com>
2021-07-22 22:02:34 +0530

    Create UI Request Sent UI

--- app/src/main/java/com/app/evolve/CreateAccount.kt
+++ app/src/main/java/com/app/evolve/CreateAccount.kt
@@ -1,7 +1,10 @@
 package com.app.evolve
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.widget.AppCompatEditText
 import com.app.evolve.customviews.AsteriskPasswordTransformationMethod
 
@@ -10,10 +13,16 @@ class CreateAccount : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
 
-        val password = findViewById<AppCompatEditText>(R.id.et_password)
+        val password = findViewById<EditText>(R.id.et_password)
         password.transformationMethod = AsteriskPasswordTransformationMethod()
 
-        val et_confirm_password = findViewById<AppCompatEditText>(R.id.et_confirm_password)
+        val et_confirm_password = findViewById<EditText>(R.id.et_confirm_password)
         et_confirm_password.transformationMethod = AsteriskPasswordTransformationMethod()
+
+        findViewById<TextView>(R.id.tv_request_access).setOnClickListener {
+            val intent = Intent(this@CreateAccount, RequestSent::class.java)
+            startActivity(intent)
+        }
+
     }
 }
