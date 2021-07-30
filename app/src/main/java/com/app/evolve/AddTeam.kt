aa193a9d1109eae78f7e032164446871bb101e7b
ashimsaini <ashimsaini37@gmail.com>
2021-07-30 11:33:43 +0530

    ADD team

---
+++ app/src/main/java/com/app/evolve/AddTeam.kt
@@ -0,0 +1,11 @@
+package com.app.evolve
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddTeam : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_team)
+    }
+}
