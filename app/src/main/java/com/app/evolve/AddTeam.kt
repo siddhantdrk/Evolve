7a1b7d04513dae720fa61bbcaf3fd0d7b66d02f1
ashimsaini <ashimsaini37@gmail.com>
2021-08-03 20:30:59 +0530

    ADD team

--- app/src/main/java/com/app/evolve/AddTeam.kt
+++ app/src/main/java/com/app/evolve/AddTeam.kt
@@ -1,11 +1,24 @@
 package com.app.evolve
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.card.MaterialCardView
+
 
 class AddTeam : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_team)
+
+        findViewById<MaterialCardView>(R.id.buttonAddTeam).setOnClickListener {
+            showBottomSheetDialog()
+        }
+    }
+
+    private fun showBottomSheetDialog() {
+        val bottomSheetDialog = BottomSheetDialog(this)
+        bottomSheetDialog.setContentView(R.layout.select_team)
+        bottomSheetDialog.show()
     }
 }
