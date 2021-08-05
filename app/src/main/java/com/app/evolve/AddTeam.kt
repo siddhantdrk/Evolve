42e358d27fcacaf574037894088e54fa1896f365
ashimsaini <ashimsaini37@gmail.com>
2021-08-05 16:54:14 +0530

    ADD team

--- app/src/main/java/com/app/evolve/AddTeam.kt
+++ app/src/main/java/com/app/evolve/AddTeam.kt
@@ -1,8 +1,13 @@
 package com.app.evolve
 
+import android.hardware.display.DisplayManager
 import android.os.Bundle
 import android.util.DisplayMetrics
+import android.view.Display
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.getSystemService
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.card.MaterialCardView
 
@@ -19,12 +24,16 @@ class AddTeam : AppCompatActivity() {
 
     private fun showBottomSheetDialog() {
         val metrics = DisplayMetrics()
-        this.windowManager.defaultDisplay.getMetrics(metrics)
+        val displayManager = getSystemService<DisplayManager>()!!
+        val defaultDisplay = displayManager.getDisplay(Display.DEFAULT_DISPLAY)
+        defaultDisplay.getRealMetrics(metrics)
         val height =  metrics.heightPixels
-        val bottomSheetDialog = BottomSheetDialog(this)
+        val bottomSheetDialog = BottomSheetDialog(this, R.style.SheetDialog)
         bottomSheetDialog.setContentView(R.layout.select_team)
-        bottomSheetDialog.behavior.peekHeight = height - 160
-        //bottomSheetDialog.behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        val constrainLayout = bottomSheetDialog.findViewById<ConstraintLayout>(R.id.constrainLayout)
+        constrainLayout!!.minHeight = height
+        bottomSheetDialog.behavior.isFitToContents = false
+        bottomSheetDialog.behavior.state = BottomSheetBehavior.STATE_EXPANDED
         bottomSheetDialog.show()
     }
 }
