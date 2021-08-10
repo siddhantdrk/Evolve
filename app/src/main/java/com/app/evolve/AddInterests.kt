3c93f97eda22c16f53451fa056871f1ab62dfcbd
ashimsaini <ashimsaini37@gmail.com>
2021-08-10 21:03:45 +0530

    Add interest

---
+++ app/src/main/java/com/app/evolve/AddInterests.kt
@@ -0,0 +1,13 @@
+package com.app.evolve
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.util.*
+import kotlin.collections.HashMap
+
+class AddInterests : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_interests)
+    }
+}
