2e6a23b794fa8fe7731ab0549151433c0ae58cdb
ashimsaini <ashimsaini37@gmail.com>
2021-08-11 18:06:40 +0530

    Add vibe

---
+++ app/src/main/java/com/app/evolve/AddVides.kt
@@ -0,0 +1,11 @@
+package com.app.evolve
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddVides : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_vides)
+    }
+}
