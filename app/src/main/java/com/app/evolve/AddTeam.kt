ec4972c447f7e9c2fac286885c9f1e72ddf2b9a1
ashimsaini <ashimsaini37@gmail.com>
2021-08-19 20:54:32 +0530

    QR code

--- app/src/main/java/com/app/evolve/AddTeam.kt
+++ app/src/main/java/com/app/evolve/AddTeam.kt
@@ -6,6 +6,7 @@ import android.util.DisplayMetrics
 import android.view.Display
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.core.content.getSystemService
 import androidx.viewpager2.widget.ViewPager2
 import com.app.evolve.adapter.viewpager.ViewPagerAdapter
@@ -13,6 +14,7 @@ import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.card.MaterialCardView
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
 import com.google.android.material.tabs.TabLayoutMediator
 
 
@@ -35,7 +37,7 @@ class AddTeam : AppCompatActivity() {
         val bottomSheetDialog = BottomSheetDialog(this, R.style.SheetDialog)
         bottomSheetDialog.setContentView(R.layout.qr_code)
         val constrainLayout = bottomSheetDialog.findViewById<ConstraintLayout>(R.id.constrainLayout)
-        initQRCode(bottomSheetDialog)
+        initQRCode(bottomSheetDialog, constrainLayout)
         constrainLayout!!.minHeight = height
         bottomSheetDialog.behavior.isFitToContents = false
         bottomSheetDialog.behavior.state = BottomSheetBehavior.STATE_EXPANDED
@@ -43,11 +45,36 @@ class AddTeam : AppCompatActivity() {
 
     }
 
-    private fun initQRCode(bottomSheetDialog: BottomSheetDialog) {
+    private fun initQRCode(
+        bottomSheetDialog: BottomSheetDialog,
+        constrainLayout: ConstraintLayout?
+    ) {
         val qrTabs = bottomSheetDialog.findViewById<TabLayout>(R.id.qrTabs)
         val adapter = ViewPagerAdapter(this)
         val viewPager = bottomSheetDialog.findViewById<ViewPager2>(R.id.viewPager)
         viewPager?.adapter = adapter
+        qrTabs!!.addOnTabSelectedListener(object : OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                /*if (tab.position == 0) {
+                    constrainLayout!!.setBackgroundColor(
+                        ContextCompat.getColor(
+                            this@AddTeam,
+                            R.color.black_alpha_70
+                        )
+                    )
+                } else {
+                    constrainLayout!!.setBackgroundColor(
+                        ContextCompat.getColor(
+                            this@AddTeam,
+                            R.color.app_background
+                        )
+                    )
+                }*/
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {}
+            override fun onTabReselected(tab: TabLayout.Tab) {}
+        })
         TabLayoutMediator(
             qrTabs!!, viewPager!!
         ) { tab, position ->
