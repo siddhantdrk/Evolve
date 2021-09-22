a94118c63722ad44851fdc36f170ff6a890ebc45
Siddhant Khobragade <siddhantdrk@gmail.com>
2021-09-22 13:29:48 +0530

    activity_forgot_password.xml updated

--- app/src/main/java/com/app/evolve/ForgetPassword.kt
+++ app/src/main/java/com/app/evolve/ForgetPassword.kt
@@ -1,11 +1,11 @@
 package com.app.evolve
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class ForgetPassword : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_forget_password)
+        setContentView(R.layout.activity_forgot_password)
     }
 }
