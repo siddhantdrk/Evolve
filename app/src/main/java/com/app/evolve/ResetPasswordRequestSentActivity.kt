15964a1e816b9184b7f74f4ac8a3786c671dd18b
Siddhant Khobragade <siddhantdrk@gmail.com>
2021-09-22 14:33:09 +0530

    activity_create_new_password.xml added

--- app/src/main/java/com/app/evolve/ResetPasswordRequestSentActivity.kt
+++ app/src/main/java/com/app/evolve/ResetPasswordRequestSentActivity.kt
@@ -3,9 +3,9 @@ package com.app.evolve
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class ForgetPassword : AppCompatActivity() {
+class ResetPasswordRequestSentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_forgot_password)
+        setContentView(R.layout.activity_reset_password_request_sent)
     }
 }
