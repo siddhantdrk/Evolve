81a940ea25e93630f28b4aed7de6c051e033e25a
Siddhant Khobragade <siddhantdrk@gmail.com>
2021-10-06 16:33:38 +0530

    activity_who_is_your_agent.xml added

---
+++ app/src/main/java/com/app/evolve/ui/WhoIsYourAgentActivity.kt
@@ -0,0 +1,12 @@
+package com.app.evolve.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.app.evolve.R
+
+class WhoIsYourAgentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_who_is_your_agent)
+    }
+}
