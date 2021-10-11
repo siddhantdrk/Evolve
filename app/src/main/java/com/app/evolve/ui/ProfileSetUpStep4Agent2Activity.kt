e7b8154a4ecc02f8c2a43ab93b45018d1374e9f4
Siddhant Khobragade <siddhantdrk@gmail.com>
2021-10-11 18:31:20 +0530

    activity_profile_set_up_step4_agent2.xml added

---
+++ app/src/main/java/com/app/evolve/ui/ProfileSetUpStep4Agent2Activity.kt
@@ -0,0 +1,12 @@
+package com.app.evolve.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.app.evolve.R
+
+class ProfileSetUpStep4Agent2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_set_up_step4_agent2)
+    }
+}
