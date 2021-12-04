afc3d59356118069bb8ecceb732ad50aae50e3f1
Siddhant Khobragade <siddhantdrk@gmail.com>
2021-12-04 22:41:43 +0530

    fragment_player_profile_info.xml contact section as a static updated

--- app/src/main/java/com/app/evolve/fragments/PlayerProfileInfoFragment.kt
+++ app/src/main/java/com/app/evolve/fragments/PlayerProfileInfoFragment.kt
@@ -1,6 +1,5 @@
 package com.app.evolve.fragments
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,11 +8,10 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.app.evolve.R
-import com.app.evolve.adapter.AddExperienceAdapter;
-import com.app.evolve.adapter.AddEducationAdapter;
-import com.app.evolve.adapter.AddAccomplishmentsAdapter;
-import com.app.evolve.adapter.AddAthletesAdapter;
-import com.app.evolve.adapter.AddContactAdapter;
+import com.app.evolve.adapter.AddAccomplishmentsAdapter
+import com.app.evolve.adapter.AddAthletesAdapter
+import com.app.evolve.adapter.AddEducationAdapter
+import com.app.evolve.adapter.AddExperienceAdapter
 
 
 class PlayerProfileInfoFragment:Fragment(){
@@ -22,7 +20,6 @@ class PlayerProfileInfoFragment:Fragment(){
     lateinit var rvEducation : RecyclerView
     lateinit var rvAccomplishments: RecyclerView
     lateinit var rvAthletes : RecyclerView
-    lateinit var rvContact : RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,18 +40,15 @@ class PlayerProfileInfoFragment:Fragment(){
         rvEducation = view.findViewById(R.id.rvEducation)
         rvAccomplishments = view.findViewById(R.id.rvAccomplishments)
         rvAthletes = view.findViewById(R.id.rvAthletes)
-        rvContact = view.findViewById(R.id.rvContact)
 
         rvExperience.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.VERTICAL ,false)
         rvEducation.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.VERTICAL ,false)
         rvAccomplishments.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.VERTICAL ,false)
         rvAthletes.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL ,false)
-        rvContact.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.VERTICAL ,false)
 
         rvExperience.adapter = AddExperienceAdapter(requireContext())
         rvEducation.adapter = AddEducationAdapter(requireContext())
         rvAccomplishments.adapter = AddAccomplishmentsAdapter(requireContext())
         rvAthletes.adapter = AddAthletesAdapter(requireContext())
-        rvContact.adapter = AddContactAdapter(requireContext())
     }
 }
