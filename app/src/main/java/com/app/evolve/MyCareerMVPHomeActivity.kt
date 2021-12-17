3421997f6e9eb7a2e44d59f8cb47e1b2fb967587
siddhantdrk <siddhantdrk@gmail.com>
2021-12-17 14:50:46 +0530

    21 and 46 screen done

---
+++ app/src/main/java/com/app/evolve/MyCareerMVPHomeActivity.kt
@@ -0,0 +1,11 @@
+package com.app.evolve
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyCareerMVPHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_career_m_v_p_home)
+    }
+}
---
+++ app/src/main/java/com/app/evolve/MyCareerMgmtTeam1Activity.kt
@@ -0,0 +1,11 @@
+package com.app.evolve
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyCareerMgmtTeam1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_career_mgmt_team1)
+    }
+}
