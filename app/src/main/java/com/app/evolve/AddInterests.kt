f3a2bee1da9b7fea8d9c94992857ba22d1bc4510
siddhantdrk <siddhantdrk@gmail.com>
2021-12-17 12:47:01 +0530

    checking push

--- app/src/main/java/com/app/evolve/AddInterests.kt
+++ app/src/main/java/com/app/evolve/AddInterests.kt
@@ -9,6 +9,5 @@ class AddInterests : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_interests)
-        //checking push
     }
 }
